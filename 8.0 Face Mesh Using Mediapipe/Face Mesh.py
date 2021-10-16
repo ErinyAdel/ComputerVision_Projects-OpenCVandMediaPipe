c81fc0f1d5d66181b02916094c850d2e7dce39b1
ErinyAdel <renyadel7@gmail.com>
2021-10-16 15:31:20 +0200

    Finished

--- 8.0 Face Mesh Using Mediapipe/Face Mesh.py
+++ 8.0 Face Mesh Using Mediapipe/Face Mesh.py
@@ -4,3 +4,32 @@ Created on Sat Oct 16 14:46:31 2021
 @author: Eriny
 """
 
+import cv2
+import mediapipe as mp
+
+img = cv2.imread('face1.jpg')
+h, w, _ = img.shape
+
+import numpy as np
+black_img = np.zeros([h,w,3], np.uint8)
+
+## Face Mesh
+face_mesh_mp = mp.solutions.face_mesh.FaceMesh()
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+result = face_mesh_mp.process(rgb)
+
+for facial_landmarks in result.multi_face_landmarks:
+    for i in range(0, 468):
+        pt1 = facial_landmarks.landmark[i]
+        #print(pt1)
+        x = int(pt1.x * w)
+        y = int(pt1.y * h)
+        cv2.circle(img, (x,y), 2, (100, 100, 0), -1)
+        cv2.circle(black_img, (x,y), 2, (0,100,0), -1)
+        
+cv2.imshow("Frame", img)
+cv2.imshow("Frame2", black_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+        
